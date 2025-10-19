4276eb4be3487cdda886f419524ad07f8b0c5834
Denis-7242 <dexdenis3@gmail.com>
2025-10-19 19:27:23 +0300

    added a file "Displayinfo.java"

---
+++ Displayinfo.java
@@ -0,0 +1,11 @@
+public class Displayinfo {
+    public static void main(String[] args) {
+        String name = "Denis Murithi";
+        int age = 18;
+        String hobby = "Coding and  exploring new technologies";
+
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " + age);
+        System.out.println("My hobby is: " + hobby);
+    }
+}
