ddf3c01feaaa03e60ebf435298c60dbe0992fc19
Denis-7242 <dexdenis3@gmail.com>
2025-10-28 21:35:21 +0300

    Added a program dealing on Assignment Operators

---
+++ AssignmentOperators.java
@@ -0,0 +1,13 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+
+        a += 5;  // a = 15
+        a -= 3;  // a = 12
+        a *= 2;  // a = 24
+        a /= 4;  // a = 6
+        a %= 5;  // a = 1
+
+        System.out.println("Final value of a: " + a);
+    }
+}
