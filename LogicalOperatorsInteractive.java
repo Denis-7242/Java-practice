d8ad6448264971f7d82ad5d1ed5ebc9d218a29a6
Denis-7242 <dexdenis3@gmail.com>
2025-10-30 17:22:46 +0300

    added a program covering the interactive part of logical operators

---
+++ LogicalOperatorsInteractive.java
@@ -0,0 +1,4 @@
+/*Here’s an interactive version of the Java Logical Operators program — it uses the Scanner 
+class so the user can input values like age, ID possession, ticket availability, and weather 
+to see how logical operators work in real time. */
+
