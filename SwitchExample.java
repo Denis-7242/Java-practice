31d599ba8fcfe03644644c4d452601f1ff9b2b73
Denis-7242 <dexdenis3@gmail.com>
2025-10-30 17:36:56 +0300

    added a file on switch statements

---
+++ SwitchExample.java
@@ -0,0 +1,4 @@
+/*Java Switch Statement
+
+The switch statement is an alternative to using many if...else if conditions.
+It’s used when you want to execute different code blocks based on a single variable’s value. */
