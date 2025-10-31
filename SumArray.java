a75a65c3f727e5913fd63b51f5bdb67c8a797c68
Denis-7242 <dexdenis3@gmail.com>
2025-11-01 00:08:54 +0300

    added a program on sum arrays

---
+++ SumArray.java
@@ -0,0 +1,14 @@
+//Find the Sum of Array Elements
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25};
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        System.out.println("Sum of elements = " + sum);
+    }
+}
