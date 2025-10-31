0f8d6f83f282390ff2f3e825f923b51596b5d58e
Denis-7242 <dexdenis3@gmail.com>
2025-10-31 23:37:18 +0300

    Added a program of Array Using for-each Loop

---
+++ ForEachArray.java
@@ -0,0 +1,12 @@
+//Array Using for-each Loop
+
+public class ForEachArray {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange"};
+
+        System.out.println("Fruits in the basket:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
