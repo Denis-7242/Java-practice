6861bfb0f5bbc741109fa16694a112bab7ecf3b0
Denis-7242 <dexdenis3@gmail.com>
2025-10-31 19:48:52 +0300

    added code on for loops

---
+++ ForLoopExample.java
@@ -0,0 +1,22 @@
+/*The for Loop
+ What it does:
+
+The for loop is used when you know how many times you want to repeat a block of code.
+It’s best for counting or iterating through things (like numbers or array elements). */
+
+// 💡 Syntax:
+/*for (initialization; condition; update) {
+    // code to be executed
+}*/
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
+//💡 What You Learn from This:
+/*How to set up and use a for loop
+How the initialization, condition, and update parts work together 
+How to control the number of iterations in a loop */
