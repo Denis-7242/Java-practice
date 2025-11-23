d0ee73914edf442cd7cf21033995b37268133afa
Denis-7242 <dexdenis3@gmail.com>
2025-11-23 15:08:58 +0300

    started revision of java

---
+++ second revision/Area.java
@@ -0,0 +1,11 @@
+public class Area{
+    public static void main(String []args){
+        int length=10;
+        int width=6;
+        int area=length*width;
+
+        System.out.println("length:" +length);
+        System.out.println("width:" +width);
+        System.out.println("area:" +area);
+    }
+}
